45f0d64c4d9aaed4809caeead2776e914050e056
angel <mer23661@uvg.edu.gt>
2024-01-29 19:48:49 -0600

    gnome sort finalizado

---
+++ GnomeSort.java
@@ -0,0 +1,25 @@
+package HDT3;
+
+public class GnomeSort implements ISort {
+
+    @Override
+    public int[] sort(int[] arr) {
+        int index = 0;
+        int n = arr.length;
+
+        while (index < n) {
+            if (index == 0)
+                index++;
+            if (arr[index] >= arr[index - 1])
+                index++;
+            else {
+                int temp = 0;
+                temp = arr[index];
+                arr[index] = arr[index - 1];
+                arr[index - 1] = temp;
+                index--;
+            }
+        }
+        return arr;
+    }
+}
