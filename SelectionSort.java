d6f7a585bcad23269209253f933f0917b8b2c507
angel <mer23661@uvg.edu.gt>
2024-01-31 19:18:32 -0600

    Algoritmo creado selection sort

---
+++ SelectionSort.java
@@ -0,0 +1,31 @@
+package HDT3;
+/**
+ * Clase que implementa un ordenamiento bubble sort
+ * @author SaintPage, colaborativo
+ */
+public class SelectionSort implements ISort{
+    @Override
+    public int[] sort(int[] arr) {
+        int n = arr.length;
+        for (int i = 0; i < n - 1; i++) {
+            int min_idx = i;
+            for (int j = i + 1; j < n; j++) {
+                if (arr[j] < arr[min_idx]) {
+                    min_idx = j;
+                }
+            }
+            int temp = arr[min_idx];
+            arr[min_idx] = arr[i];
+            arr[i] = temp;
+        }
+        return arr;
+    }
+
+    public void printArray(int[] arr) {
+        int n = arr.length;
+        for (int i = 0; i < n; ++i) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
+}
